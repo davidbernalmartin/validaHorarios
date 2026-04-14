6618dc5fa7a4568a3cff09cd8a87e0d19df6e71c
davidbernalmartin <david.bernal.martin@gmail.com>
2026-04-14 13:04:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from datetime import timedelta
 from supabase import create_client, Client
 
 # --- CONFIGURACIÓN DE CONEXIÓN ---
-SUPABASE_URL = "TU_URL_DE_PROYECTO"
-SUPABASE_KEY = "TU_API_KEY_ANON"
+SUPABASE_URL = "https://kmoisrouwjxpzqngqryu.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imttb2lzcm91d2p4cHpxbmdxcnl1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxNjQwNjYsImV4cCI6MjA5MTc0MDA2Nn0.2XasZwY54wKxOH_tzgcKzCqZUqy--PA8XsBxZACIkug"
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
 st.set_page_config(page_title="Gestor de Sedes Supabase", page_icon="⚽", layout="wide")
